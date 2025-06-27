c6933f2d9dbe6fda993a3bfa7319b49d303f4c13
Lineked <lineked@yandex.ru>
2025-06-27 17:05:53 +0300

    .

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-    model = YOLO("yolo11_cbam_wiou.pt")
 
-    # results = model.train(
-    #     data="data.yaml",
-    #     epochs=300,
-    #     imgsz=640,
-    #     device=0,
-    #     batch=16,
-    #     name="test",
-    #     optimizer='SGD'
-    # )
+    model = YOLO("cbam_2.yaml")
+    results = model.train(
+        data="data.yaml",
+        epochs=300,
+        imgsz=640,
+        device=0,
+        batch=16,
+        name="yolo_cbam",
+        optimizer='SGD'
+    )
 
-    # Run validation
-    metrics = model.val(split = 'val')
+    # validation
+    metrics = model.val(split = 'test')
     print(f"mAP50: {metrics.box.map50}")
     print(f"mAP50-95: {metrics.box.map}")
+    model.save("yolo_cbam2.pt")
